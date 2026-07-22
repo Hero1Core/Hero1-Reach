464e9db5891dcebe51bc03d1533ec894d0d98615
Hero1 <heroeliteg251@gmail.com>
2026-07-22 19:03:53 +0300

    Create base.py for database model imports
    Added base.py for SQLAlchemy model imports.

---
+++ backend/app/db/base.py
@@ -0,0 +1,7 @@
+from app.db.base_class import Base
+
+# Import all SQLAlchemy models here.
+# Example:
+# from app.models.user import User
+# from app.models.channel import Channel
+# from app.models.video import Video
