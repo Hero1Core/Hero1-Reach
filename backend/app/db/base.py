bafaada4f0fe759d2959a9f02cdbac4672fafadd
Hero1 <heroeliteg251@gmail.com>
2026-07-23 10:52:53 +0300

    Import User model in base.py

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -1,7 +1,2 @@
 from app.db.base_class import Base
-
-# Import all SQLAlchemy models here.
-# Example:
-# from app.models.user import User
-# from app.models.channel import Channel
-# from app.models.video import Video
+from app.models.user import User
