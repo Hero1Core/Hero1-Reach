363a4d57a07ab9542ad835a15ffb243c66118223
Hero1 <heroeliteg251@gmail.com>
2026-07-23 11:00:40 +0300

    Add new model imports to base.py

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -2,3 +2,8 @@ from app.db.base_class import Base
 
 from app.models.user import User
 from app.models.workspace import Workspace
+from app.models.project import Project
+from app.models.chat import Chat
+from app.models.message import Message
+from app.models.file import File
+from app.models.subscription import Subscription
